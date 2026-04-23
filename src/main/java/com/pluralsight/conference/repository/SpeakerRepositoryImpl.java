608dfc587a37b4b803901896b29b4bcb2c33436b
MiriamDP <miriam101d@gmail.com>
2026-04-23 09:20:35 +0200

    simple jdbc

--- src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.pluralsight.conference.repository;
 import com.pluralsight.conference.model.Speaker;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository("speakerRepository")
 public class SpeakerRepositoryImpl implements SpeakerRepository {
@@ -29,7 +32,22 @@ public class SpeakerRepositoryImpl implements SpeakerRepository {
     @Override
     public Speaker create(Speaker speaker) {
 
-        jdbcTemplate.update("INSERT INTO speaker (name) VALUES (?)",speaker.getName());
+        //jdbcTemplate.update("INSERT INTO speaker (name) VALUES (?)",speaker.getName());
+
+        //para hacerlo sin sql
+        SimpleJdbcInsert insert=new SimpleJdbcInsert(jdbcTemplate);
+        insert.setTableName("speaker");
+        List<String> columns=new ArrayList<>();
+        columns.add("name");
+
+        Map<String, Object> data=new HashMap<>();
+        data.put("name", speaker.getName());
+
+        insert.setGeneratedKeyName("id");
+
+        Number key=insert.executeAndReturnKey(data);
+
+        System.out.println(key);
 
 
         return null;
