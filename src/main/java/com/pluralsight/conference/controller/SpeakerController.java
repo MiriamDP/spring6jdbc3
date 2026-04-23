a884ecbf518e2b420b7eba0aab92b4406387e7fd
MiriamDP <miriam101d@gmail.com>
2026-04-23 08:32:14 +0200

    test

--- src/main/java/com/pluralsight/conference/controller/SpeakerController.java
+++ src/main/java/com/pluralsight/conference/controller/SpeakerController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class SpeakerController {
@@ -16,7 +19,15 @@ public class SpeakerController {
         this.speakerService = speakerService;
     }
 
-    @GetMapping
+    //no se ha puesto a nivel de controlador la ruta porque lo mismo controlamos mas recursos con este controller
+    @PutMapping("/speaker")
+    public Speaker createSpeaker(@RequestBody Speaker speaker) {
+
+        System.out.println("Name: "+speaker.getName());
+        return null;
+    }
+
+    @GetMapping("/speaker")
     public List<Speaker> getSpeakers() {
         return speakerService.findAll();
     }
