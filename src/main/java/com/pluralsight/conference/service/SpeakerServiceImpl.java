c1000c6e804396eaa76b645a5277f66312e123fb
MiriamDP <miriam101d@gmail.com>
2026-04-27 08:21:37 +0200

    transacciones

--- src/main/java/com/pluralsight/conference/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/conference/service/SpeakerServiceImpl.java
@@ -3,7 +3,9 @@ package com.pluralsight.conference.service;
 import com.pluralsight.conference.model.Speaker;
 import com.pluralsight.conference.repository.SpeakerRepository;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +40,7 @@ public class SpeakerServiceImpl implements SpeakerService {
     }
 
     @Override
+    @Transactional
     public void batch() {
         List<Speaker> speakers=speakerRepository.findAll();
 
@@ -49,6 +52,10 @@ public class SpeakerServiceImpl implements SpeakerService {
         }
 
         speakerRepository.update(pairs);
+        //ya autolanza el rollback
+        throw new DataAccessException("Error in batch") {
+            
+        };
     }
 
     @Override
